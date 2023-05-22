63b386b4f1cb276e06a21eb9961dab62736df8a7
msaqibdev <msaqibdev@gmail.com>
2023-05-23 00:04:31 +0500

    useLocation Hook

---
+++ src/components/LocationHook.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import useLocation from "../hooks/useLocation";
+
+const LocationHook = () => {
+  const { location } = useLocation();
+  console.log("🚀 ~ file: LocationHook.jsx:6 ~ LocationHook ~ location:", location)
+  return (
+    <>
+     <p>{location?.latitude}</p>
+     <p>{location?.longitude}</p>
+    </>
+  );
+};
+
+export default LocationHook;
